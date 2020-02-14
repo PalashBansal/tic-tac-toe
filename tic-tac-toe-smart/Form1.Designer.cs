6cd182f4f087b83e905c2ab4f8061a86e4678b36
Palash Bansal <palash.bansal20@gmail.com>
2020-02-14 18:26:56 +0530

    lot of improvement

--- tic-tac-toe-smart/Form1.Designer.cs
+++ tic-tac-toe-smart/Form1.Designer.cs
@@ -32,13 +32,19 @@
             this.label1 = new System.Windows.Forms.Label();
             this.button2_AImove = new System.Windows.Forms.Button();
             this.gameStatus = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1_newgame
             // 
-            this.button1_newgame.Location = new System.Drawing.Point(268, 99);
+            this.button1_newgame.Location = new System.Drawing.Point(2, 2);
+            this.button1_newgame.Margin = new System.Windows.Forms.Padding(2);
             this.button1_newgame.Name = "button1_newgame";
-            this.button1_newgame.Size = new System.Drawing.Size(122, 56);
+            this.button1_newgame.Size = new System.Drawing.Size(92, 24);
             this.button1_newgame.TabIndex = 0;
             this.button1_newgame.Text = "New game";
             this.button1_newgame.UseVisualStyleBackColor = true;
@@ -47,17 +53,19 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(28, 612);
+            this.label1.Location = new System.Drawing.Point(2, 51);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(138, 17);
+            this.label1.Size = new System.Drawing.Size(102, 13);
             this.label1.TabIndex = 1;
             this.label1.Text = "First turn is always \' \'";
             // 
             // button2_AImove
             // 
-            this.button2_AImove.Location = new System.Drawing.Point(31, 633);
+            this.button2_AImove.Location = new System.Drawing.Point(2, 67);
+            this.button2_AImove.Margin = new System.Windows.Forms.Padding(2);
             this.button2_AImove.Name = "button2_AImove";
-            this.button2_AImove.Size = new System.Drawing.Size(107, 29);
+            this.button2_AImove.Size = new System.Drawing.Size(92, 24);
             this.button2_AImove.TabIndex = 1;
             this.button2_AImove.Text = "AI First";
             this.button2_AImove.UseVisualStyleBackColor = true;
@@ -66,26 +74,85 @@
             // gameStatus
             // 
             this.gameStatus.AutoSize = true;
-            this.gameStatus.Location = new System.Drawing.Point(217, 724);
+            this.gameStatus.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.gameStatus.Location = new System.Drawing.Point(2, 94);
+            this.gameStatus.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.gameStatus.Name = "gameStatus";
-            this.gameStatus.Size = new System.Drawing.Size(173, 17);
+            this.gameStatus.Size = new System.Drawing.Size(131, 13);
             this.gameStatus.TabIndex = 2;
             this.gameStatus.Text = "Status: Game Not Started!";
             // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Increment = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Location = new System.Drawing.Point(68, 30);
+            this.numericUpDown1.Margin = new System.Windows.Forms.Padding(2);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            9,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(26, 20);
+            this.numericUpDown1.TabIndex = 3;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            this.numericUpDown1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.numericUpDown1_KeyPress);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(2, 32);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "GameSize:";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button1_newgame);
+            this.panel1.Controls.Add(this.gameStatus);
+            this.panel1.Controls.Add(this.numericUpDown1);
+            this.panel1.Controls.Add(this.button2_AImove);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(11, 562);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(150, 123);
+            this.panel1.TabIndex = 5;
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(782, 753);
-            this.Controls.Add(this.gameStatus);
-            this.Controls.Add(this.button2_AImove);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.button1_newgame);
+            this.ClientSize = new System.Drawing.Size(984, 845);
+            this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Tic-Tac-Toe-Smart";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -95,6 +162,9 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button2_AImove;
         private System.Windows.Forms.Label gameStatus;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel1;
     }
 }
 
--- tic-tac-toe-smart/Form1.cs
+++ tic-tac-toe-smart/Form1.cs
@@ -17,12 +17,12 @@ namespace tic_tac_toe_smart
         {
             InitializeComponent();
         }
-  
+
         public class CustomButton : Button
         {
-            const int buttonsize=80;
-            public const int Width = buttonsize;
-            public const int Height = buttonsize;
+            const int buttonsize = 80;
+            public new const int Width = buttonsize;
+            public new const int Height = buttonsize;
 
             public CustomButton()
             {
@@ -35,39 +35,74 @@ namespace tic_tac_toe_smart
             }
         }
 
-        const int GRIDSIZE = 3;
+        const int MAXGRIDSIZE = 9;
+        int GRIDSIZE = 3;
         //const int GRIDSIZE2D = GRIDSIZE * GRIDSIZE;
-        CustomButton[,] gridButton = new CustomButton[GRIDSIZE, GRIDSIZE];
-        int[,] gridnum = new int[GRIDSIZE, GRIDSIZE]; // 'X' is 1 and 'O' is 2
-        enum turns {X, O};
-        const turns gameStartsWith = turns.O;
+        CustomButton[,] gridButton = new CustomButton[MAXGRIDSIZE, MAXGRIDSIZE];
+        int[,] gridnum = new int[MAXGRIDSIZE, MAXGRIDSIZE]; // 'X' is 1 and 'O' is 2
+        enum turns { X, O };
+        const turns gameStartsWith = turns.O;// opposite of game starting actually with
         turns lastTurn = gameStartsWith;
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            label1.Text = "First turn is always" + (lastTurn.Equals(turns.O)? "'X'" : "'O'");
+            label1.Text = "First turn is always" + (lastTurn.Equals(turns.O) ? "'X'" : "'O'");
             System.Windows.Forms.ToolTip buttonAImovetip = new System.Windows.Forms.ToolTip();
             buttonAImovetip.SetToolTip(button2_AImove, "Make AI play first");
-            int buttonBasePosH = this.ClientSize.Height / 2 - CustomButton.Height * 2;
-            int buttonBasePosW = this.ClientSize.Width / 2 - CustomButton.Width * 2;
-            for (int x = 0; x < GRIDSIZE; ++x)
-                for (int y = 0; y < GRIDSIZE; ++y)
+            makeButtonGrid();
+            showGrid();
+        }
+
+        private void makeButtonGrid()
+        {
+            for (int x = 0; x< MAXGRIDSIZE; ++x)
+                for (int y = 0; y< MAXGRIDSIZE; ++y)
                 {
                     gridnum[x, y] = 0;
                     gridButton[x, y] = new CustomButton()
                     {
                         Name = String.Format("customButton{0}{1}", x, y),
-                        TabIndex = 100 + GRIDSIZE * x + y,
-                        Location = new Point(buttonBasePosW + CustomButton.Width * x,
-                                                 buttonBasePosH + CustomButton.Height * y)
+                        TabIndex = 100 + MAXGRIDSIZE * x + y
                     };
                     gridButton[x, y].Click  += new EventHandler(gridButton_Click);
+                    gridButton[x, y].Hide();
                     Controls.Add(gridButton[x, y]);
                 }
-                    
+        }
+
+        private void showGrid()
+        {
+            // initial location
+            int buttonBasePosH = CustomButton.Height / 2;
+            int buttonBasePosW = (int)(CustomButton.Width * 2.5);
+            // location based on grid size
+            buttonBasePosH += (9-GRIDSIZE)* CustomButton.Height/2;
+            showBoundaryIndexes();
+            for (int x = 0; x < MAXGRIDSIZE; ++x)
+                for (int y = 0; y < MAXGRIDSIZE; ++y)
+                {
+                    gridButton[x, y].Hide();
+                }
+            for (int x = 0; x < GRIDSIZE; ++x)
+                for (int y = 0; y < GRIDSIZE; ++y)
+                {
+                    gridButton[x, y].Show();
+                    gridButton[x, y].Location = new Point(buttonBasePosW + CustomButton.Width * x,
+                                                 buttonBasePosH + CustomButton.Height * y);
+                }
+        }
+
+        private void showBoundaryIndexes() //to be implemented
+        {
+            ///also show A-I and 1-9 on grid boundary rows and columns
         }
 
         private void button_newgame(object sender, EventArgs e)
+        {
+            newgame();
+        }
+
+        private void newgame()
         {
             for (int x = 0; x < GRIDSIZE; ++x)
                 for (int y = 0; y < GRIDSIZE; ++y)
@@ -85,37 +120,59 @@ namespace tic_tac_toe_smart
             Button button = sender as Button;
             int gridX = button.Name[12]-48;
             int gridY = button.Name[13]-48;
-            if (lastTurn == turns.X)
+            if (gridnum[gridX, gridY] == 0)
             {
-                button.Text = "O";
-                lastTurn = turns.O;
-                gridnum[gridX, gridY] = 2;
+                if (lastTurn == turns.X)
+                {
+                    button.Text = "O";
+                    lastTurn = turns.O;
+                    gridnum[gridX, gridY] = 2;
+                }
+                else
+                {
+                    button.Text = "X";
+                    lastTurn = turns.X;
+                    gridnum[gridX, gridY] = 1;
+                }
+                button2_AImove.Enabled = false;
+                checkWhoWon();
             }
-            else
-            {
-                button.Text = "X";
-                lastTurn = turns.X;
-                gridnum[gridX, gridY] = 1;
-            }
-            button2_AImove.Enabled = false;
-            checkWhoWon();
         }
 
         enum GameStatus { User, AI, Draw, NoOne, GameNotStarted};
 
-        private void checkWhoWon()
+        private void checkWhoWon() //to be implemented
         {
             GameStatus whoWon = GameStatus.GameNotStarted;
             ///who won check here
             /// all has value and sum=3 means X win; sum=6 means O win, otherwise no win. All occupied but no sum=3/6 means draw
             /// user should get 'game draw' message asap, if no possible next moves can give any one win scenario. for eg-
             /*
-            O.x
-            xoo
-            O.x
-            none of x or o can win. as 'o' can't put 'o' in both the empty places left too. so immediately put status as 'game draw'
-            and make grid disable for user input, until user presses new game button.
+                gridnum indexes->
+                00 10 20
+                01 11 21
+                02 12 22
+                
+                O.x
+                xoo
+                O.x
+                none of x or o can win. as 'o' can't put 'o' in both the empty places left too. so immediately put status as 'game draw'
+                and make grid disable for user input, until user presses new game button.
             */
+            int x = 0, y = 0;
+            for(x = 0; x < GRIDSIZE; ++x)
+            {
+                for (y = 1; y < GRIDSIZE; ++y)
+                {
+                    if (gridnum[x, 0]==0 || gridnum[x, 0] != gridnum[x, y])
+                        break;
+                }
+                if (y == GRIDSIZE)
+                {
+                    //whoWon = gridnum[x, 0]==1?gameStatus.;
+                    break;
+                }
+            }
             ///
             alertUser(whoWon);
         }
@@ -167,12 +224,25 @@ namespace tic_tac_toe_smart
             checkWhoWon();
         }
 
-        private void AImove()
+        private void AImove() //to be implemented
         {
             ///move by AI code here
             ///refer https://puzzling.stackexchange.com/questions/30/what-is-the-optimal-first-move-in-tic-tac-toe
             ///if first move- go in corner
             ///if 2nd move, then if first move is corner, go for centre, otherwise...
+        }
+        
+
+        private void numericUpDown1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = true;
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            GRIDSIZE = (int)numericUpDown1.Value;
+            showGrid();
+            newgame();
         }
     }
 }
