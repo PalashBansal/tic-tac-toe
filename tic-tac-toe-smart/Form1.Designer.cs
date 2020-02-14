25d386270610bf38890292818a9a5ac0b181e26d
Bansal <palash.bansal@siemens-healthineers.com>
2020-02-14 11:24:38 +0530

    adding initial files

---
+++ tic-tac-toe-smart/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace tic_tac_toe_smart
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1_newgame = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2_AImove = new System.Windows.Forms.Button();
+            this.gameStatus = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1_newgame
+            // 
+            this.button1_newgame.Location = new System.Drawing.Point(268, 99);
+            this.button1_newgame.Name = "button1_newgame";
+            this.button1_newgame.Size = new System.Drawing.Size(122, 56);
+            this.button1_newgame.TabIndex = 0;
+            this.button1_newgame.Text = "New game";
+            this.button1_newgame.UseVisualStyleBackColor = true;
+            this.button1_newgame.Click += new System.EventHandler(this.button_newgame);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 612);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(138, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "First turn is always \' \'";
+            // 
+            // button2_AImove
+            // 
+            this.button2_AImove.Location = new System.Drawing.Point(31, 633);
+            this.button2_AImove.Name = "button2_AImove";
+            this.button2_AImove.Size = new System.Drawing.Size(107, 29);
+            this.button2_AImove.TabIndex = 1;
+            this.button2_AImove.Text = "AI First";
+            this.button2_AImove.UseVisualStyleBackColor = true;
+            this.button2_AImove.Click += new System.EventHandler(this.button2_AImove_Click);
+            // 
+            // gameStatus
+            // 
+            this.gameStatus.AutoSize = true;
+            this.gameStatus.Location = new System.Drawing.Point(217, 724);
+            this.gameStatus.Name = "gameStatus";
+            this.gameStatus.Size = new System.Drawing.Size(173, 17);
+            this.gameStatus.TabIndex = 2;
+            this.gameStatus.Text = "Status: Game Not Started!";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(782, 753);
+            this.Controls.Add(this.gameStatus);
+            this.Controls.Add(this.button2_AImove);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1_newgame);
+            this.Name = "Form1";
+            this.Text = "Tic-Tac-Toe-Smart";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1_newgame;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2_AImove;
+        private System.Windows.Forms.Label gameStatus;
+    }
+}
+
---
+++ tic-tac-toe-smart/Form1.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace tic_tac_toe_smart
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+  
+        public class CustomButton : Button
+        {
+            const int buttonsize=80;
+            public const int Width = buttonsize;
+            public const int Height = buttonsize;
+
+            public CustomButton()
+            {
+                BackColor = Color.FromArgb(200, 200, 200);
+                ForeColor = Color.Black;
+                Font = new Font(Font.FontFamily, 40);
+                FlatStyle = FlatStyle.Flat;
+                Size = new Size(Width, Height);
+                UseVisualStyleBackColor = false;
+            }
+        }
+
+        const int GRIDSIZE = 3;
+        //const int GRIDSIZE2D = GRIDSIZE * GRIDSIZE;
+        CustomButton[,] gridButton = new CustomButton[GRIDSIZE, GRIDSIZE];
+        int[,] gridnum = new int[GRIDSIZE, GRIDSIZE]; // 'X' is 1 and 'O' is 2
+        enum turns {X, O};
+        turns lastTurn = turns.O;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label1.Text = "First turn is always" + (lastTurn.Equals(turns.O)? "'X'" : "'O'");
+            System.Windows.Forms.ToolTip buttonAImovetip = new System.Windows.Forms.ToolTip();
+            buttonAImovetip.SetToolTip(button2_AImove, "Make AI play first");
+            int buttonBasePosH = this.ClientSize.Height / 2 - CustomButton.Height * 2;
+            int buttonBasePosW = this.ClientSize.Width / 2 - CustomButton.Width * 2;
+            for (int x = 0; x < GRIDSIZE; ++x)
+                for (int y = 0; y < GRIDSIZE; ++y)
+                {
+                    gridnum[x, y] = 0;
+                    gridButton[x, y] = new CustomButton()
+                    {
+                        Name = String.Format("customButton{0}{1}", x, y),
+                        TabIndex = 100 + GRIDSIZE * x + y,
+                        Location = new Point(buttonBasePosW + CustomButton.Width * x,
+                                                 buttonBasePosH + CustomButton.Height * y)
+                    };
+                    gridButton[x, y].Click  += new EventHandler(gridButton_Click);
+                    Controls.Add(gridButton[x, y]);
+                }
+                    
+        }
+
+        private void button_newgame(object sender, EventArgs e)
+        {
+            for (int x = 0; x < GRIDSIZE; ++x)
+                for (int y = 0; y < GRIDSIZE; ++y)
+                {
+                    gridButton[x, y].ResetText();
+                    gridnum[x, y] = 0;
+                }
+            button2_AImove.Enabled = true;
+            alertUser(GameStatus.GameNotStarted);
+        }
+
+        private void gridButton_Click(object sender, EventArgs e)
+        {
+            Button button = sender as Button;
+            int gridX = button.Name[12]-48;
+            int gridY = button.Name[13]-48;
+            if (lastTurn == turns.X)
+            {
+                button.Text = "O";
+                lastTurn = turns.O;
+                gridnum[gridX, gridY] = 2;
+            }
+            else
+            {
+                button.Text = "X";
+                lastTurn = turns.X;
+                gridnum[gridX, gridY] = 1;
+            }
+            button2_AImove.Enabled = false;
+            checkWhoWon();
+        }
+
+        enum GameStatus { User, AI, Draw, NoOne, GameNotStarted};
+
+        private void checkWhoWon()
+        {
+            GameStatus whoWon = GameStatus.GameNotStarted;
+            ///who won check here
+            /// all has value and sum=3 means X win; sum=6 means O win, otherwise no win. All occupied but no sum=3/6 means draw
+            
+            alertUser(whoWon);
+        }
+       
+
+        private void alertUser(GameStatus whoWon)
+        {
+            switch(whoWon)
+            {
+                case GameStatus.GameNotStarted:
+                    gameStatus.Text = "Status: Game Not Started!";
+                    break;
+                case GameStatus.NoOne:
+                    gameStatus.Text = "Status: Game in Progress!";
+                    break;
+                case GameStatus.Draw:
+                    gameStatus.Text = "Status : Match Draw!";
+                    break;
+                case GameStatus.User:
+                    gameStatus.Text = "Status : You Won!";
+                    break;
+                case GameStatus.AI:
+                    gameStatus.Text = "Status : AI Won!";
+                    break;
+            }
+        }
+
+        private void button2_AImove_Click(object sender, EventArgs e)
+        {
+            bool notFirstMove = false;
+            button2_AImove.Enabled = false;
+            for (int x = 0; x < GRIDSIZE; ++x)
+            {
+                for (int y = 0; y < GRIDSIZE; ++y)
+                {
+                    if (gridnum[x, y] != 0)
+                    {
+                        notFirstMove = true;
+                        break;
+                    }
+                }
+                if (notFirstMove)
+                    break;
+            }
+            if(!notFirstMove)
+            {
+                AImove();
+            }
+            checkWhoWon();
+        }
+
+        private void AImove()
+        {
+            ///move by AI code here
+            ///refer https://puzzling.stackexchange.com/questions/30/what-is-the-optimal-first-move-in-tic-tac-toe
+            ///if first move- go in corner
+            ///if 2nd move, then if first move is corner, go for centre, otherwise...
+        }
+    }
+}
